188cd0883ed8077c1001ed63d8853caf9450c87d
chenL <776446020@qq.com>
2022-03-27 10:54:05 +0800

    继续添加、以及new了一个对象

--- src/main/java/com/catl/git/HelloGit.java
+++ src/main/java/com/catl/git/HelloGit.java
@@ -4,5 +4,8 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello git First");
         System.out.println("更新1");
+
+        System.out.println("继续添加......");
+        HelloGit helloGit = new HelloGit();
     }
 }
